81c9169fac196d6b6f0af1a279df82219870e5b1
Mihajlo-Chonev <mc291218@ohio.edu>
2020-09-04 14:54:18 -0400

    new branch file

---
+++ die-main.cc
@@ -0,0 +1,33 @@
+/*
+* Author: Mihajlo
+* Date: 09/04/2020
+* Description: A program that creates a die, says what its face value is, creates another die with 12 sides,
+               checks out its face value, then switches the die to 24 sides, and proceeds to roll it 6 times.
+*/
+
+
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include "die.h"
+using namespace std;
+
+
+
+//function prototypes
+int main() {
+    Die d;
+
+    cout << "Face value: "<< d << endl;
+    Die d12(12);
+
+    cout << "Face value: "<< d12 << endl;
+    d12.setNumSides(24);
+
+    for (size_t i = 0; i < 6; i++) {
+        cout << "Roll: " << d12.roll() << endl;
+    }
+
+    return 0;
+}
